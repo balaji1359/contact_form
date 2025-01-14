d0b3957c98f98207fa39cc6abafb7b5740509c8a
Balaji Nagisetty <nagisetty.balaji@gmail.com>
2025-01-14 18:36:55 +0100

    terraform apply correction.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "balaji-nagisetty"
+    key    = "contact_form.tfstate"
+    region = "us-east-1"
+  }
+}
